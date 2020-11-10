edd4688b19899cc6bf9aea33b3a4abc966deb2a7
Paul Williams <paul58914080@gmail.com>
2020-11-10 09:02:05 +0530

    styles: configured prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import "./App.scss";
+import React, { ReactNode } from 'react';
+import './App.scss';
 
-function App(): any {
-  return <div>Test</div>;
+class App extends React.PureComponent {
+  public render(): ReactNode {
+    return (
+      <div className="w-100 d-flex align-items-center justify-content-center">
+        <h1>Welcome !! Hello, world</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
